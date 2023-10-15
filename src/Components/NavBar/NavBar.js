3ebd79320511d3ff7a2d6b7569b96197f094df05
Sue Raisianzadeh <49556690+sue-raisianzadeh@users.noreply.github.com>
2023-10-16 12:00:21 +1300

    Update NavBar.js

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,5 @@
 import './NavBar.css';
 
-// import React, { useState } from "react";
-
-// const NavBar = () => {
-  // // const [isOpen, setOpen] = useState(false);
-  // return (
-  //   <nav
-  //     className="NavBar"
-  //     role="navigation"
-  //     aria-label="main navigation"
-  //   >
-  //     <div className="NavBar">
-  //       <div className="NavBar">
-  //         <a href="" role="button" aria-label="menu"
-  //           aria-expanded="true"
-  //           // onClick={() => setOpen(!isOpen)}>
-  //         </a>
-  //       </div>
   function NavBar() {
     return (
      
